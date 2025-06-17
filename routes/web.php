88e455f0bca002a11511c9e7f3725582226e39c9
zidnafaz <zidnafaza27@gmail.com>
2025-06-17 20:44:25 +0700

    'Update dashboard for DOS'

--- routes/web.php
+++ routes/web.php
@@ -395,7 +395,7 @@ Route::middleware('auth')->group(function () {
     });
 
     // Route dashboard
-    Route::prefix('chart')->name('chart.')->middleware('authorize:ADM,VAL,ANG')->group(function () {
+    Route::prefix('chart')->name('chart.')->middleware('authorize:ADM,VAL,ANG,DOS')->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('index');
         Route::get('/more-info', [DashboardController::class, 'moreInfo'])->name('moreInfo');
     });
